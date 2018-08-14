5022586c3d676c2e3f5dc62c20232f949910f02b
Mirko Krause <mirko.krause@student.hpi.de>
2018-08-14 22:10:32 +0200

    log to file and stderr

--- Main.go
+++ Main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"io"
+	"log"
+	"os"
 	"tg-bot-endless-story/common"
 )
+
+func logIntoFile() {
+	logfile, err := os.OpenFile(common.Conf.DataDirectory+common.Conf.LogFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	common.Check(err)
+
+	mw := io.MultiWriter(os.Stderr, logfile)
+	log.SetOutput(mw)
+}
 
 func main() {
 	println("Opening the Book...")
 
 	common.ReadConfig()
 	common.InitializeStorage()
+	logIntoFile()
 
 	println(common.Conf.Language.CliWelcome)
 
